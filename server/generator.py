7534ee064108681bdddd36dbfeb78ea23a01b013
Gleb Stepanov <glebstepanov1992@gmail.com>
2016-06-05 14:39:12 +0300

    Charts for multiple device on single page added.

--- server/generator.py
+++ server/generator.py
@@ -5,7 +5,7 @@ import time
 
 BASE_URL = "http://0.0.0.0:9000"
 URL = "/rs/data/post/{0}/{1}"
-DEVICE_ID = "abcd"
+devices = ["abcd", "sdzgf", "ssdgv"]
 
 
 def main():
@@ -13,17 +13,18 @@ def main():
     deviation = 30
 
     while True:
-        gpio = "0000"
-        voltage = "00" + hex(base_Value +
-                             random.randint(0, deviation) - deviation/ 2)[2:]
-        power = "AAAA"
-        temperature = "BBBB"
-        data = gpio + voltage + power + temperature + "0000" * 4
+        for device in devices:
+            gpio = "0000"
+            voltage = "00" + hex(base_Value +
+                                 random.randint(0, deviation) - deviation/ 2)[2:]
+            power = "AAAA"
+            temperature = "BBBB"
+            data = gpio + voltage + power + temperature + "0000" * 4
 
-        url = BASE_URL + URL.format(DEVICE_ID, data)
-        response = requests.get(url)
-        print("Status code: {0}".format(response.status_code))
-        time.sleep(1)
+            url = BASE_URL + URL.format(device, data)
+            response = requests.get(url)
+            print("Status code: {0}".format(response.status_code))
+            time.sleep(1)
 
 
 if __name__ == "__main__":
