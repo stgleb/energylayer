dbb0c64ec52b7bed4085d7799d81a74f3d9b5c3b
Gleb Stepanov <glebstepanov1992@gmail.com>
2016-06-14 00:06:44 +0300

    oauth config added.

---
+++ server/oauth/oauth_config.py
@@ -0,0 +1,25 @@
+TWITTER_CONSUMER_KEY = ''
+TWITTER_SECRET_KEY = ''
+TWITTER_BASE_URL = 'https://api.twitter.com/1.1'
+TWITTER_AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+TWITTER_REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+TWITTER_ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+TWITTER_AUTHENTICATE_URL = 'https://api.twitter.com/oauth/authenticate'
+
+FACEBOOK_BASE_URL = 'https://graph.facebook.com/'
+FACEBOOK_ACCESS_TOKEN_URL = 'oauth/access_token'
+FACEBOOK_AUTHORIZE_URL = 'https://www.facebook.com/dialog/oauth'
+FACEBOOK_CONSUMER_KEY = ''
+FACEBOOK_SECRET_KEY = ''
+FACEBOOK_REQUEST_TOKEN_PARAMS = {'scope': 'email'}
+FACEBOOK_REFRESH_URL = 'https://graph.facebook.com/oauth/access_token'
+FACEBOOK_API_VERSION = '/v2.5/'
+
+GOOGLE_CONSUMER_KEY = ''
+GOOGLE_SECRET_KEY = ''
+GOOGLE_SCOPE = ['https://www.googleapis.com/auth/userinfo.email',
+                'https://www.googleapis.com/auth/userinfo.profile']
+GOOGLE_TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
+GOOGLE_AUTH_URL = 'https://accounts.google.com/o/oauth2/auth'
+GOOGLE_BASE_URL = 'https://www.googleapis.com/oauth2/v1'
+GOOGLE_REDIRECT_URI = 'http://localhost:9000'
