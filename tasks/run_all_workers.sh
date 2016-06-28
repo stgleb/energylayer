3d2d0aed1cea94c77a5ee9ab5bfd26f5b599ca82
Ubuntu <gleb@gleb.gleb-11b2m0tl.b5.internal.cloudapp.net>
2016-06-28 09:17:39 +0000

    Fix run_all_workers

--- tasks/run_all_workers.sh
+++ tasks/run_all_workers.sh
@@ -1,3 +1,3 @@
 screen -S hour celery -A aggregate worker -B -l info -Q hour -n worker1
 screen -S day celery -A aggregate worker -B -l info -Q day -n worker2
-screen -S hour celery -A aggregate worker -B -l info -Q hour -n worker1
+screen -S week celery -A aggregate worker -B -l info -Q week -n worker3
