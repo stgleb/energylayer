e186e78847d9fff001f54c55c4045ccb393fb289
Gleb Stepanov <gstepanov@mirantis.com>
2016-09-08 21:05:45 +0300

    Remove oauth data

--- server/oauth/oauth_config.py
+++ server/oauth/oauth_config.py
@@ -1,5 +1,5 @@
-TWITTER_CONSUMER_KEY = 'yUkYGKgWyk6SozNRHrDrrc87E'
-TWITTER_SECRET_KEY = 'uZg29QUwbhm8J2xjaeQ09YtY18R6L1lXsiy5D9oi4uuL4tXoqb'
+TWITTER_CONSUMER_KEY = ''
+TWITTER_SECRET_KEY = ''
 TWITTER_BASE_URL = 'https://api.twitter.com/1.1'
 TWITTER_AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
 TWITTER_REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
@@ -9,14 +9,14 @@ TWITTER_AUTHENTICATE_URL = 'https://api.twitter.com/oauth/authenticate'
 FACEBOOK_BASE_URL = 'https://graph.facebook.com/'
 FACEBOOK_ACCESS_TOKEN_URL = 'oauth/access_token'
 FACEBOOK_AUTHORIZE_URL = 'https://www.facebook.com/dialog/oauth'
-FACEBOOK_CONSUMER_KEY = '820328301430267'
-FACEBOOK_SECRET_KEY = '7f1621930eb91c4a59bbb1c46e2de5e1'
+FACEBOOK_CONSUMER_KEY = ''
+FACEBOOK_SECRET_KEY = ''
 FACEBOOK_REQUEST_TOKEN_PARAMS = {'scope': 'email'}
 FACEBOOK_REFRESH_URL = 'https://graph.facebook.com/oauth/access_token'
 FACEBOOK_API_VERSION = '/v2.5/'
 
-GOOGLE_CONSUMER_KEY = '493739144798-tqieclh8bqkekkjgeujg6c7it3mgtquj.apps.googleusercontent.com'
-GOOGLE_SECRET_KEY = 'KuA4ywmrbJtlfm1X6mTQlnK-'
+GOOGLE_CONSUMER_KEY = ''
+GOOGLE_SECRET_KEY = ''
 GOOGLE_SCOPE = ['https://www.googleapis.com/auth/userinfo.email',
                 'https://www.googleapis.com/auth/userinfo.profile']
 GOOGLE_TOKEN_URL = 'https://accounts.google.com/o/oauth2/token'
